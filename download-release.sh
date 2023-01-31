131abe6926ab6dffc4d4529d428ae8e0615703fb
超神的冰凉 <2801621420@qq.com>
2023-01-31 15:06:12 +0800

    适配Jitpack *5

--- download-release.sh
+++ download-release.sh
@@ -2,23 +2,23 @@ mkdir -p "target"
 cd "target" || exit 1
 
 # check whether FrostRender.jar exists
-if [ ! -f "libdeflate-java-0.0.2-PNX" ]; then
-    echo "libdeflate-java-0.0.2-PNX not found, downloading from GitHub..."
+if [ ! -f "libdeflate-java-0.0.2-PNX.jar" ]; then
+    echo "libdeflate-java-0.0.2-PNX.jar not found, downloading from GitHub..."
 
     download_url=$(curl -Ls "https://api.github.com/repos/PowerNukkitX/libdeflate-java/releases/latest" \
       | grep -Eo '"browser_download_url": "https:.+?"' \
       | head -n 1 \
       | grep -Eo 'https:.+?\.jar')
 
-    rm -f "libdeflate-java-0.0.2-PNX"
-    echo "Downloading libdeflate-java-0.0.2-PNX from $download_url..."
+    rm -f "libdeflate-java-0.0.2-PNX.jar"
+    echo "Downloading libdeflate-java-0.0.2-PNX.jar from $download_url..."
 
     if [ -z "$HTTP_PROXY" ]; then
         echo "No proxy detected. You can set proxy via HTTP_PROXY environment variable."
-        curl -# -Ls -o "libdeflate-java-0.0.2-PNX" "$download_url"
+        curl -Ls -o "libdeflate-java-0.0.2-PNX.jar" "$download_url"
     else
         echo "Using proxy $HTTP_PROXY"
-        curl -# -Ls -x "$HTTP_PROXY" -o "libdeflate-java-0.0.2-PNX" "$download_url"
+        curl -Ls -x "$HTTP_PROXY" -o "libdeflate-java-0.0.2-PNX.jar" "$download_url"
     fi
 
     ls
