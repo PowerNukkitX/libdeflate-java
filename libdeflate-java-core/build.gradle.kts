b2544b349ad224cc64608823a645145f30b7c1b5
tunikakeks <54219265+tunikakeks@users.noreply.github.com>
2026-03-16 12:49:35 +0100

    Refactor build.gradle.kts for better readability

--- libdeflate-java-core/build.gradle.kts
+++ libdeflate-java-core/build.gradle.kts
@@ -11,18 +11,14 @@ plugins {
 group = "org.powernukkitx"
 version = "0.0.3.PNX-SNAPSHOT"
 
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
-
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.0\"")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
 }
 
-tasks.register("compileNatives") {
+task("compileNatives") {
+    // Note: we should prefer compilation with GCC
     val jniTempPath = Paths.get(project.rootDir.toString(), "tmp")
 
     doLast {
@@ -31,12 +27,17 @@ tasks.register("compileNatives") {
 
         when {
             Os.isFamily(Os.FAMILY_MAC) -> {
-                if (System.getenv("CC") == null) env["CC"] = "clang"
+                // macOS is just a Unix like the rest.
+                if (System.getenv("CC") == null) {
+                    env["CC"] = "clang"
+                }
+
                 val byteOut = ByteArrayOutputStream()
                 project.exec {
                     commandLine = listOf("/usr/libexec/java_home")
                     standardOutput = byteOut
                 }
+
                 env["JAVA_HOME"] = byteOut.toString("UTF-8").trim()
                 env["DYLIB_SUFFIX"] = "dylib"
                 env["JNI_PLATFORM"] = "darwin"
@@ -46,14 +47,16 @@ tasks.register("compileNatives") {
 
                 exec {
                     executable = "make"
-                    args = listOf("clean", "all")
-                    environment = env
+                    args = arrayListOf("clean", "all")
+                    environment = env.toMap()
                 }
             }
-
             Os.isFamily(Os.FAMILY_UNIX) -> {
-                if (System.getenv("CC") == null) env["CC"] = "gcc"
-                val osName = System.getProperty("os.name").lowercase(Locale.ENGLISH)
+                // Cover most Unices. It's 2020, so hopefully you're compiling on a modern open-source BSD or Linux distribution...
+                if (System.getenv("CC") == null) {
+                    env["CC"] = "gcc"
+                }
+                val osName = System.getProperty("os.name").toLowerCase(Locale.ENGLISH)
                 env["DYLIB_SUFFIX"] = "so"
                 env["JNI_PLATFORM"] = osName
                 env["LIB_DIR"] = Paths.get(jniTempPath.toString(), "compiled", osName, System.getProperty("os.arch")).toString()
@@ -62,19 +65,26 @@ tasks.register("compileNatives") {
 
                 exec {
                     executable = "make"
-                    args = listOf("clean", "all")
-                    environment = env
+                    args = arrayListOf("clean", "all")
+                    environment = env.toMap()
                 }
             }
-
             Os.isFamily(Os.FAMILY_WINDOWS) -> {
                 if (System.getenv("MSVC") != null) {
+                    // Windows is a very, very special case... we compile with Microsoft Visual Studio, which is a mess.
+                    // There is the `vswhere` utility, which brings a tiny bit of sanity, but alas... it is probably better
+                    // to invoke the build from a batch script.
+                    //
+                    // We'll need `vswhere` (you can install it via Chocolatey).
                     exec {
                         executable = "cmd"
-                        args = listOf("/C", "windows_build.bat")
+                        args = arrayListOf("/C", "windows_build.bat")
                     }
                 } else {
-                    if (System.getenv("CC") == null) env["CC"] = "gcc"
+                    // Attempt to build using an MSYS2 environment.
+                    if (System.getenv("CC") == null) {
+                        env["CC"] = "gcc"
+                    }
                     env["DYLIB_SUFFIX"] = "dll"
                     env["JNI_PLATFORM"] = "win32"
                     env["LIB_DIR"] = Paths.get(jniTempPath.toString(), "compiled", "windows", System.getProperty("os.arch")).toString()
@@ -83,13 +93,15 @@ tasks.register("compileNatives") {
 
                     exec {
                         executable = "make"
-                        args = listOf("clean", "all")
-                        environment = env
+                        args = arrayListOf("clean", "all")
+                        environment = env.toMap()
                     }
                 }
-            }
 
-            else -> throw RuntimeException("Your OS isn't supported.")
+            }
+            else -> {
+                throw RuntimeException("Your OS isn't supported. We'll take a PR!")
+            }
         }
     }
 }
@@ -103,16 +115,16 @@ sourceSets {
 }
 
 tasks.named<ProcessResources>("processResources") {
-    dependsOn("compileNatives")
+    dependsOn(tasks.get("compileNatives"))
 }
 
 tasks.named<Test>("test") {
-    dependsOn("compileNatives")
+    dependsOn(tasks.get("compileNatives"))
     useJUnitPlatform()
 }
 
 tasks.jar {
-    dependsOn("compileNatives")
+    dependsOn(tasks.get("compileNatives"))
 }
 
 publishing {
