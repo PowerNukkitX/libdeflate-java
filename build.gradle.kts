996a37d100dde100ae977ff3d73ff476ce193bea
tunikakeks <54219265+tunikakeks@users.noreply.github.com>
2026-03-16 12:32:38 +0100

    Refactor build.gradle.kts for publishing configuration
    Removed Java plugin configuration and updated publication details.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    java
     `maven-publish`
 }
 
@@ -12,19 +11,34 @@ allprojects {
     }
 }
 
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
-
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            from(components["java"])
+            // publish the module that actually contains the Java classes
+            from(project(":libdeflate-java-core").components["java"])
 
             groupId = "org.powernukkitx"
             artifactId = "libdeflate-java"
-            version = "0.0.3.PNX-SNAPSHOT"
+            version = project.version.toString()
+
+            pom {
+                name.set("libdeflate-java")
+                description.set("Java bindings for libdeflate used by PowerNukkitX")
+                url.set("https://github.com/PowerNukkitX/libdeflate-java")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/PowerNukkitX/libdeflate-java.git")
+                    developerConnection.set("scm:git:ssh://github.com/PowerNukkitX/libdeflate-java.git")
+                    url.set("https://github.com/PowerNukkitX/libdeflate-java")
+                }
+            }
         }
     }
 
@@ -37,6 +51,7 @@ publishing {
                 username = providers.gradleProperty("pnxUsername")
                     .orElse(providers.environmentVariable("PNX_REPO_USERNAME"))
                     .orNull
+
                 password = providers.gradleProperty("pnxPassword")
                     .orElse(providers.environmentVariable("PNX_REPO_PASSWORD"))
                     .orNull
