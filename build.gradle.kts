84b5234c6781ecb1903178475d7651ebe2ce189d
tunikakeks <54219265+tunikakeks@users.noreply.github.com>
2026-03-16 12:06:37 +0100

    Configure Maven publishing and update project details to gradle
    Added Maven publishing configuration and updated group and version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,46 @@
 plugins {
     java
+    `maven-publish`
 }
 
-group = "cn.powernukkitx"
-version = "0.0.3-PNX"
+group = "org.powernukkitx"
+version = "0.0.3.PNX-SNAPSHOT"
 
 allprojects {
     repositories {
         mavenCentral()
+    }
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            groupId = "org.powernukkitx"
+            artifactId = "libdeflate-java"
+            version = "0.0.3.PNX-SNAPSHOT"
+        }
+    }
+
+    repositories {
+        maven {
+            name = "pnx"
+            url = uri("https://repo.powernukkitx.org/releases")
+
+            credentials {
+                username = providers.gradleProperty("pnxUsername")
+                    .orElse(providers.environmentVariable("PNX_REPO_USERNAME"))
+                    .orNull
+                password = providers.gradleProperty("pnxPassword")
+                    .orElse(providers.environmentVariable("PNX_REPO_PASSWORD"))
+                    .orNull
+            }
+        }
     }
 }
