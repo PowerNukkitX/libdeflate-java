4d8674a44b632c704ea52e8f7b829b8377a92385
tunikakeks <54219265+tunikakeks@users.noreply.github.com>
2026-03-16 12:45:15 +0100

    Update group ID and remove publishing configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,12 @@
 plugins {
     java
-    `maven-publish`
 }
 
-group = "org.powernukkitx"
+group = "cn.powernukkitx"
 version = "0.0.3.PNX-SNAPSHOT"
 
 allprojects {
     repositories {
         mavenCentral()
-    }
-}
-
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            // Publish the submodule that has Java classes
-            from(project(":libdeflate-java-core").components["java"])
-
-            groupId = "org.powernukkitx"
-            artifactId = "libdeflate-java"
-            version = project.version.toString()
-        }
-    }
-
-    repositories {
-        maven {
-            name = "pnx"
-            url = uri("https://repo.powernukkitx.org/releases")
-
-            credentials {
-                username = providers.gradleProperty("pnxUsername")
-                    .orElse(providers.environmentVariable("PNX_REPO_USERNAME"))
-                    .orNull
-                password = providers.gradleProperty("pnxPassword")
-                    .orElse(providers.environmentVariable("PNX_REPO_PASSWORD"))
-                    .orNull
-            }
-        }
     }
 }
