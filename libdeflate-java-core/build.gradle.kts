ab082e41861a6aec8c4226e6ee384122a6f96716
Curse <anderson@lalumail.com>
2026-09-25 22:58:05 +0200

    Fix ARM64 libdeflate POSIX feature detection

--- libdeflate-java-core/build.gradle.kts
+++ libdeflate-java-core/build.gradle.kts
@@ -61,7 +61,7 @@ task("compileNatives") {
                 env["JNI_PLATFORM"] = osName
                 env["LIB_DIR"] = Paths.get(jniTempPath.toString(), "compiled", osName, System.getProperty("os.arch")).toString()
                 env["OBJ_DIR"] = Paths.get(jniTempPath.toString(), "objects", osName, System.getProperty("os.arch")).toString()
-                env["CFLAGS"] = "-O2 -fomit-frame-pointer -Werror -Wall -fPIC -flto"
+                env["CFLAGS"] = "-O2 -fomit-frame-pointer -Werror -Wall -fPIC -flto -D_POSIX_C_SOURCE=200809L"
 
                 exec {
                     executable = "make"
